2f080436b58bffc5767713b9c3c32e4ea9ad57e7
FBK <gary19920103@hotmail.com.tw>
2026-03-04 11:27:14 +0800

    v1.0

--- tools/sign.ps1
+++ tools/sign.ps1
@@ -3,8 +3,12 @@
 $cert = Get-ChildItem Cert:\LocalMachine\My\ -CodeSigningCert | Select-Object -First 1
 
 # 2. 先確保檔案是帶 BOM 的 UTF-8 格式 (防止簽署時轉換失敗)
-$content = Get-Content -Path "main.ps1" -Raw
-$content | Out-File -FilePath "main.ps1" -Encoding utf8
-
+$content = Get-Content -Path "machine.ps1" -Raw
+$content | Out-File -FilePath "machine.ps1" -Encoding utf8
 # 3. 對腳本進行簽署
-Set-AuthenticodeSignature -FilePath "main.ps1" -Certificate $cert
+Set-AuthenticodeSignature -FilePath "machine.ps1" -Certificate $cert
+
+
+$content = Get-Content -Path "user.ps1" -Raw
+$content | Out-File -FilePath "user.ps1" -Encoding utf8
+Set-AuthenticodeSignature -FilePath "user.ps1" -Certificate $cert
